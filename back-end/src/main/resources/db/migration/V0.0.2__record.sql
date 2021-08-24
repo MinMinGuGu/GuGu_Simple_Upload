b19858f0a753a24aa4416406d214d56ae6545eb0
minmin <newmailbyminmin@gmail.com>
2021-08-24 23:08:36 +0800

    修复sql错误

--- back-end/src/main/resources/db/migration/V0.0.2__record.sql
+++ back-end/src/main/resources/db/migration/V0.0.2__record.sql
@@ -1,5 +1,5 @@
-create database if not exists `visit`(
-    `id` int auto_increment not null comment 'id',
+create table if not exists `visit`(
+    `id` int auto_increment not null ,
     `ip_address` varchar(100) not null comment 'ip地址',
     `path` varchar(255) not null comment '访问路径',
     `create_time` datetime not null comment '创建时间',
