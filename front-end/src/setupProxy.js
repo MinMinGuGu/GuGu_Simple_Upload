b3d731526bffdc18812cfc4a76202ea56da79652
minmin <newmailbyminmin@gmail.com>
2021-10-31 16:05:13 +0800

    - 添加注释备忘 - 前端配置文件错误修复

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -1,20 +1,18 @@
-import apis from './config/setting'
-
 const proxy = require('http-proxy-middleware')
 
 // todo 思考如何在前端自定义环境变量
 const env = process.env.NODE_env;
 let serverPort;
 if (env === 'dev') {
-    serverPort = apis.devPort
+    serverPort = 80
 } else {
-    serverPort = apis.proPort
+    serverPort = 8841
 }
 
 module.exports = function (app) {
     app.use(
         proxy('/api', {// 前缀请求触发
-            target: apis.backendUri + ':' + apis.defaultPort, // 转发地址
+            target: 'http://localhost:80', // 转发地址
             changeDrigin: true, // 控制服务器收到的响应头中Host字段的值
             pathRewrite: { '^/api': '/' }
         })
