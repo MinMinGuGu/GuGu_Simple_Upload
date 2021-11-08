7c7a526972ed2367ebaf73ee759c77da69398c2f
minmin <newmailbyminmin@gmail.com>
2021-11-08 21:52:15 +0800

    - 添加缺失的前端文件

---
+++ front-end/src/config/setting.js
@@ -0,0 +1,13 @@
+// 接口前缀  不要修改
+const apiPrefix = "/api"
+// 接口配置
+let apis = {
+    defaultPort: 80,
+    devPort: 80,
+    proPort: 8841,
+    backendUri: 'http://localhost',
+    loginApi: apiPrefix + "/login",
+    fileApi: apiPrefix + "/file",
+}
+
+export default apis
