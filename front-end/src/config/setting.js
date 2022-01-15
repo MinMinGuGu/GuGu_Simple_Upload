757697c684d82a919f92b64fdb827ef561065b1b
minmin <newmailbyminmin@gmail.com>
2022-01-15 12:56:03 +0800

    - 配置暂存

--- front-end/src/config/setting.js
+++ front-end/src/config/setting.js
@@ -1,10 +1,11 @@
+console.log('process.env', process.env)
+console.log('process.argv', process.argv)
 // 接口前缀  不要修改
 const apiPrefix = "/api"
 // 接口配置
 let apis = {
-    defaultPort: 80,
     devPort: 80,
-    proPort: 8841,
+    proPort: 8848,
     backendUri: 'http://localhost',
     loginApi: apiPrefix + "/login",
     fileApi: apiPrefix + "/file",
