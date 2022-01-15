8b22028bd6c5acd4e4e93d73a9a1d562db3ee4f2
minmin <newmailbyminmin@gmail.com>
2022-01-15 21:19:08 +0800

    - 配置开发时的动态代理根据运行环境代理到指定URL

--- front-end/src/config/setting.js
+++ front-end/src/config/setting.js
@@ -1,15 +1,10 @@
-const env = process.env.APP_ENV
+const env = process.env.APP_ENV;
 const apiPrefix = "/api";
-const rootFlag = "//"
-const port = env === 'dev' ? 80 : 8848;
-const requestHost = rootFlag + "localhost" + ":" + port;
-// 接口配置
+
 let apis = {
-    devPort: 80,
-    proPort: 8848,
-    loginApi: requestHost + apiPrefix + "/login",
-    fileApi: requestHost + apiPrefix + "/file",
-    systemApi: requestHost + apiPrefix + "/system",
+    loginApi: apiPrefix + "/login",
+    fileApi: apiPrefix + "/file",
+    systemApi: apiPrefix + "/system",
     env
 }
 
--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -1,18 +1,18 @@
 const proxy = require('http-proxy-middleware')
 
-// todo 思考如何在前端自定义环境变量
-const env = process.env.NODE_env;
-let serverPort;
-if (env === 'dev') {
-    serverPort = 80
-} else {
-    serverPort = 8841
-}
+const env = process.env.APP_ENV;
+const httpArgee = "http://";
+const portEnv = {
+    "dev": 80,
+    "pro": 8848
+};
+const port = portEnv[env];
+const requestHost = httpArgee + "localhost:" + port;
 
 module.exports = function (app) {
     app.use(
         proxy('/api', {// 前缀请求触发
-            target: 'http://localhost:80', // 转发地址
+            target: requestHost, // 转发地址
             changeDrigin: true, // 控制服务器收到的响应头中Host字段的值
             pathRewrite: { '^/api': '/' }
         })
