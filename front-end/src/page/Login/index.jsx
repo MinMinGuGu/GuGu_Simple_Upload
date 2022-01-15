5fe46cd76e21b47e0fa910c23179afe61617cc51
minmin <newmailbyminmin@gmail.com>
2022-01-15 11:21:19 +0800

    - 修复登录页面无法正常跳转的问题

--- front-end/src/page/Login/index.jsx
+++ front-end/src/page/Login/index.jsx
@@ -1,48 +1,56 @@
-import React, { Component } from 'react'
-import { Alert } from 'antd'
-import './index.css'
-import './bootstrap.min.css'
-import { doPost } from '../../utils/requestUtil'
-import apis from "../../config/setting"
+import React, { Component } from "react";
+import { Alert } from "antd";
+import "./index.css";
+import "./bootstrap.min.css";
+import { doPost } from "../../utils/requestUtil";
+import apis from "../../config/setting";
+import { withRouter } from "react-router-dom";
 
-export default class Login extends Component {
-
+class Login extends Component {
     state = {
-        msg: ''
-    }
+        msg: "",
+    };
 
     postHandler = (event) => {
-        event.preventDefault()
-        const { username: { value: username }, passwrod: { value: password }, rememberMe: { checked: rememberMe } } = this
-        console.log(apis.loginApi)
-        const result = doPost(apis.loginApi, { username, password, rememberMe })
-        result.then(data => {
+        event.preventDefault();
+        const {
+            username: { value: username },
+            passwrod: { value: password },
+            rememberMe: { checked: rememberMe },
+        } = this;
+        console.log(apis.loginApi);
+        const result = doPost(apis.loginApi, {
+            username,
+            password,
+            rememberMe,
+        });
+        result.then((data) => {
             if (data.code === 200) {
-                this.props.history.push('/')
-                return
+                this.props.history.push("/");
+                return;
             }
-            this.setState({ msg: data.message })
-        })
-    }
+            this.setState({ msg: data.message });
+        });
+    };
 
     showMsg = () => {
-        if (this.state.msg !== '') {
+        if (this.state.msg !== "") {
             return (
                 <Alert
                     message="登录失败"
                     description={this.state.msg}
                     type="error"
                     closable
-                    style={{ textAlign: 'center' }}
+                    style={{ textAlign: "center" }}
                     onClose={this.onClose}
                 />
-            )
+            );
         }
-    }
+    };
 
     onClose = () => {
-        this.setState({ msg: '' })
-    }
+        this.setState({ msg: "" });
+    };
 
     render() {
         return (
@@ -56,26 +64,59 @@ export default class Login extends Component {
                                 <h1>Login</h1>
                                 <form>
                                     <div className="form-group">
-                                        <input ref={r => this.username = r} autoFocus type="text" className="form-control" id="username" name="username" placeholder="Enter username" />
+                                        <input
+                                            ref={(r) => (this.username = r)}
+                                            autoFocus
+                                            type="text"
+                                            className="form-control"
+                                            id="username"
+                                            name="username"
+                                            placeholder="Enter username"
+                                        />
                                     </div>
                                     <div className="form-group">
-                                        <input ref={r => this.passwrod = r} type="password" className="form-control" id="passwrod" placeholder="Password" />
+                                        <input
+                                            ref={(r) => (this.passwrod = r)}
+                                            type="password"
+                                            className="form-control"
+                                            id="passwrod"
+                                            placeholder="Password"
+                                        />
                                     </div>
                                     <div className="form-check">
                                         <label className="switch">
-                                            <input ref={r => this.rememberMe = r} type="checkbox" name="rememberMe" />
+                                            <input
+                                                ref={(r) =>
+                                                    (this.rememberMe = r)
+                                                }
+                                                type="checkbox"
+                                                name="rememberMe"
+                                            />
                                             <span className="slider round"></span>
                                         </label>
-                                        <label className="form-check-label" htmlFor="exampleCheck1">Remember Me</label>
+                                        <label
+                                            className="form-check-label"
+                                            htmlFor="exampleCheck1"
+                                        >
+                                            Remember Me
+                                        </label>
                                     </div>
                                     <br />
-                                    <button type="submit" onClick={this.postHandler} className="btn btn-lg btn-block btn-success">Sign in</button>
+                                    <button
+                                        type="submit"
+                                        onClick={this.postHandler}
+                                        className="btn btn-lg btn-block btn-success"
+                                    >
+                                        Sign in
+                                    </button>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+export default withRouter(Login);
