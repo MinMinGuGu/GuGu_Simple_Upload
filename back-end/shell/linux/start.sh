4152d8f05b563da2a7b2ee0b73ae327bae9bfb00
minmin <khadijajwflghb@gmail.com>
2022-09-09 00:26:25 +0800

    - 增加溢出输出堆栈信息

--- back-end/shell/linux/start.sh
+++ back-end/shell/linux/start.sh
@@ -4,6 +4,6 @@ startPath=$basePath/../../boot/GuGu_Simple_Upload.jar
 configPath=$basePath/../../config/application.yml
 logConfigPath=$basePath/../../config/logback-spring.xml
 read -rsp "Enter project key:" password
-nohup java -jar "$startPath" --jasypt.encryptor.password="$password" --spring.config.location="$configPath" --logging.config="$logConfigPath" > /dev/null 2>&1 &
+nohup java -XX:+HeapDumpOnOutOfMemoryError -jar "$startPath" --jasypt.encryptor.password="$password" --spring.config.location="$configPath" --logging.config="$logConfigPath" > /dev/null 2>&1 &
 echo $! > process.pid
 echo -e "\nGuGu_Simple_Upload Start Successful!"
--- back-end/shell/windows/start.bat
+++ back-end/shell/windows/start.bat
@@ -3,4 +3,4 @@ set startPath=%cd%/../../boot/GuGu_Simple_Upload.jar
 set configPath=%cd%/../../config/application.yml
 set logConfigPath=%cd%/../../config/logback-spring.xml
 set /p password=Enter project key:
-java -jar %startPath% --jasypt.encryptor.password=%password% --spring.config.location=%configPath% --logging.config=%logConfigPath%
+java -XX:+HeapDumpOnOutOfMemoryError -jar %startPath% --jasypt.encryptor.password=%password% --spring.config.location=%configPath% --logging.config=%logConfigPath%
