31557628a4bcbbeb8989734b7287dbb914a0e220
minmin <khadijajwflghb@gmail.com>
2023-01-04 06:33:09 +0800

    - 删除登录返回之前重定向页面的设计

--- front-end/src/layout/LeftMenu/index.jsx
+++ front-end/src/layout/LeftMenu/index.jsx
@@ -19,7 +19,7 @@ class LeftMenu extends Component {
             if (Object.hasOwnProperty.call(routers, pass)) {
                 const route = routers[pass];
                 if (route.path === localRoute) {
-                    // todo 根据redirect跳转过来时菜单能够正确显示
+                    // todo 考虑每次登录都要从概率页开始 而不是重定向
                 }
             }
         }
@@ -31,52 +31,54 @@ class LeftMenu extends Component {
     };
 
     render() {
-        const account = JSON.parse(sessionStorage.getItem("account"));
         let renderContxt = [
             <Menu.Item key="概览" icon={<PieChartOutlined />} path="/">
                 概览
             </Menu.Item>,
         ];
-        const { permissionList } = account;
-        for (let i = 0; i < permissionList.length; i++) {
-            let permission = permissionList[i];
-            if (permission.name === "upload" || permission.name === "all") {
-                renderContxt.push(
-                    <SubMenu
-                        key="文件管理"
-                        icon={<ProfileOutlined />}
-                        title="文件管理"
-                    >
-                        <Menu.Item key="文件上传" path="/file/upload">
-                            文件上传
-                        </Menu.Item>
-                        <Menu.Item key="文件列表" path="/file/list">
-                            文件列表
-                        </Menu.Item>
-                    </SubMenu>
-                );
-            }
-            if (permission.name === "manage" || permission.name === "all") {
-                renderContxt.push(
-                    <SubMenu
-                        key="系统管理"
-                        icon={<FormOutlined />}
-                        title="系统管理"
-                    >
-                        <Menu.Item key="用户管理" path="/system/user">
-                            用户管理
-                        </Menu.Item>
-                        <Menu.Item key="角色管理" path="/system/role">
-                            角色管理
-                        </Menu.Item>
-                        <Menu.Item key="AppKey设置" path="/system/appKey">
-                            AppKey设置
-                        </Menu.Item>
-                        <Menu.Item key="操作日志" path="/system/appLog">
-                            操作日志
-                        </Menu.Item>
-                    </SubMenu>
-                );
+        const account = JSON.parse(sessionStorage.getItem("account"));
+        if (account) {
+            const { permissionList } = account;
+            for (let i = 0; i < permissionList.length; i++) {
+                let permission = permissionList[i];
+                if (permission.name === "upload" || permission.name === "all") {
+                    renderContxt.push(
+                        <SubMenu
+                            key="文件管理"
+                            icon={<ProfileOutlined />}
+                            title="文件管理"
+                        >
+                            <Menu.Item key="文件上传" path="/file/upload">
+                                文件上传
+                            </Menu.Item>
+                            <Menu.Item key="文件列表" path="/file/list">
+                                文件列表
+                            </Menu.Item>
+                        </SubMenu>
+                    );
+                }
+                if (permission.name === "manage" || permission.name === "all") {
+                    renderContxt.push(
+                        <SubMenu
+                            key="系统管理"
+                            icon={<FormOutlined />}
+                            title="系统管理"
+                        >
+                            <Menu.Item key="用户管理" path="/system/user">
+                                用户管理
+                            </Menu.Item>
+                            <Menu.Item key="角色管理" path="/system/role">
+                                角色管理
+                            </Menu.Item>
+                            <Menu.Item key="AppKey设置" path="/system/appKey">
+                                AppKey设置
+                            </Menu.Item>
+                            <Menu.Item key="操作日志" path="/system/appLog">
+                                操作日志
+                            </Menu.Item>
+                        </SubMenu>
+                    );
+                }
             }
         }
         renderContxt.push(
